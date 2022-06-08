3d501724ce0dcf6b2eeffff04ed9905ca1426692
Anton <anton_pr@rambler.ru>
2022-06-08 20:57:51 +0300

    initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,40 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+require("./tasks/addVoting");
+require("./tasks/vote");
+require("./tasks/finish");
+require("./tasks/withdraw");
+require("./tasks/votingList");
+require("./tasks/proposalList");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.7.3",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: API_URL,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.4",
 };
